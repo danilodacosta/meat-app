ca0e1f037fcdbc4af2ced01ab401b77319ef3d00
danilodacosta <danilo-gc@hotmail.com>
2019-05-01 22:32:41 -0300

    criando componentes de reviews e menu.

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,4 +1,7 @@
+import { Restaurant } from './../restaurants/restaurant/restaurant.model';
 import { Component, OnInit } from '@angular/core';
+import { RestaurantsService } from './../restaurants/restaurants.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mt-restaurant-detail',
@@ -6,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantDetailComponent implements OnInit {
 
-  constructor() { }
+  public resturant: Restaurant;
+
+  constructor(private resturantService: RestaurantsService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.resturantService.restaurantById(this.route.snapshot.params['id'])
+      .subscribe(restaurant => this.resturant = restaurant);
   }
-
 }
