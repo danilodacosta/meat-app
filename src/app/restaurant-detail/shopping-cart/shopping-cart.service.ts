801c709d5a0efbd8b402a1a63382db74fa046ce6
danilodacosta <danilo-gc@hotmail.com>
2019-05-14 16:45:53 -0300

    criando modulos e animações.

--- src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -1,8 +1,13 @@
+import { NotificationService } from './../../shared/messages/notification.service';
+import { Injectable } from '@angular/core';
 import { MenuItem } from './../menu-item/menu-item.model';
 import { CartItem } from './cart-item.model';
 
+@Injectable()
 export class ShoppingCartService {
   public items: CartItem[] = [];
+
+  constructor(private notificationService: NotificationService) {}
 
   public clear(): void {
     this.items = [];
@@ -16,6 +21,8 @@ export class ShoppingCartService {
     } else {
       this.items.push(new CartItem(item));
     }
+
+    this.notificationService.notify(`Você adicionou o item ${item.name}`);
   }
 
   increaseQty(item: CartItem) {
@@ -24,6 +31,7 @@ export class ShoppingCartService {
 
   public removeItem(item: CartItem): void {
     this.items.splice(this.items.indexOf(item), 1);
+    this.notificationService.notify(`Você removeu o item ${item.menuItem.name}`);
   }
 
   decreaseQty(item: CartItem) {
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,30 @@
+import { NotificationService } from './messages/notification.service';
+import { RestaurantsService } from './../restaurants/restaurants.service';
+import { ShoppingCartService } from './../restaurant-detail/shopping-cart/shopping-cart.service';
+import { OrderService } from './../order/order.service';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { RatingComponent } from './rating/rating.component';
+import { InputComponent } from './input/input.component';
+import { RadioComponent } from './radio/radio.component';
+import { SnackbarComponent } from './messages/snackbar/snackbar.component';
+
+@NgModule({
+  declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
+  imports:      [CommonModule, FormsModule, ReactiveFormsModule],
+  exports:      [InputComponent, RadioComponent,
+                 RatingComponent, CommonModule, SnackbarComponent,
+                 FormsModule, ReactiveFormsModule ]
+})
+
+export class SharedModule {
+ static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [OrderService, ShoppingCartService, RestaurantsService, NotificationService]
+    };
+ }
+}
